25c306298745e2614bd6e1e59348d98b33747e7e
prakohad@gmail.com <prakohad@gmail.com>
2018-10-24 17:34:02 +0530

    merged

--- springBootTeamProject/src/main/java/com/client/Mycontroller.java
+++ springBootTeamProject/src/main/java/com/client/Mycontroller.java
@@ -37,7 +37,7 @@ public class Mycontroller {
 		 System.out.println("Prashant Pardhi  work on git hub");
 		 System.out.println("merge test1");  
 		 System.out.println("test for merge conflict");
-		 return "login";
+		 return "login"; 
 		 }  
 	 
 	 @RequestMapping(value="/register")public String goToRegistrationPage() {return "registration";} 
