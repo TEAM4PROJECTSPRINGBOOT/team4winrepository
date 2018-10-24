a6e8791606efbbb91f478e26f0ab43abd5a730ba
prakohad@gmail.com <prakohad@gmail.com>
2018-10-24 17:35:09 +0530

    conflict

--- springBootTeamProject/src/main/java/com/client/Mycontroller.java
+++ springBootTeamProject/src/main/java/com/client/Mycontroller.java
@@ -36,7 +36,7 @@ public class Mycontroller {
 		 System.out.println("Samirrrrrrrrrrrr");
 		 System.out.println("Prashant Pardhi  work on git hub");
 		 System.out.println("merge test1");  
-		 System.out.println("test for merge conflict");
+		 System.out.println("test for merge conflict");    
 		 return "login"; 
 		 }  
 	 
