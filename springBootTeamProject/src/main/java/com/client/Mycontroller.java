a28024e0e38b3daa6dc7dd4363396edc06475bf5
akashdharmale123 <akashdharmale123@gmail.com>
2018-10-24 09:52:55 +0530

    m

--- springBootTeamProject/src/main/java/com/client/Mycontroller.java
+++ springBootTeamProject/src/main/java/com/client/Mycontroller.java
@@ -35,10 +35,7 @@ public class Mycontroller {
 		 System.out.println("in Login..");
 
 		 System.out.println("Akash is working on GitHub..");
-		 System.out.println("pranay kohad");
-		 System.out.println("pranay kohad is projects admin");
-		 System.out.println("Samirrrrrrrrrrrr");
-		 System.out.println("Prashant Pardhi  work on git hub developer ...........");
+		 System.out.println("try to merge akashbranch -- master..");
 
 		 return "login";
 		 }  
